424b036190d9962e675001579779551c52318889
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2021-08-30 14:10:02 +0000

    Sync LeetCode submission - Sort Colors (cpp)

---
+++ my-folder/problems/sort_colors/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int i = 0;
+        int j = 0;
+        int k = nums.size()-1;
+        
+        while(j<=k)
+        {
+            if(nums[j]==0)
+            {
+               swap(nums[i++],nums[j++]);
+            } 
+            else if(nums[j]==1)
+            {
+                j++;
+            }
+            else if(nums[j]==2)
+            {
+                swap(nums[j],nums[k--]);
+            }
+        }
+    }
+};
