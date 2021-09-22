dbc5d654ae49caf86476e148bd9ae2b3b48c6b4d
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2021-09-22 06:39:37 +0000

    Sync LeetCode submission - Single Number II (cpp)

---
+++ my-folder/problems/single_number_ii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> m;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        int ans = 0;
+        for(auto i:m)
+        {
+            if(i.second==1)
+            {
+                ans = i.first;
+            }
+        }
+        return ans;
+    }
+};
