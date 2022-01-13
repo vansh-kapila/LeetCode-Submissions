e94216410e5cd36090d5f56b235ddeac87774a42
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-01-13 08:17:05 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- my-folder/problems/fibonacci_number/solution.cpp
+++ my-folder/problems/fibonacci_number/solution.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        int arr[31];
-        arr[0]=0;
-        arr[1]=1;
-        arr[2]=1;
-        
-        for(int i=2;i<31;i++)
+        if(n==0)
         {
-            arr[i]=arr[i-1]+arr[i-2];
+            return 0;
         }
-        return arr[n];
         
+        int dp[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
     }
 };
