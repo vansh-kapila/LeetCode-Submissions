1ecc89915dbf274936243d916a716d63e70d4cf5
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-02-17 03:07:19 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- my-folder/problems/combination_sum/solution.cpp
+++ my-folder/problems/combination_sum/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-   
-    void helper(vector<vector<int>> &ans,vector<int> candidates,int target,int sum,int i,vector<int> x)
+    vector<vector<int>> ans;
+    void helper(vector<int> &candidates,int target,int sum,int i,vector<int> x)
     {
         if(sum==target)
         {
@@ -12,17 +12,16 @@ public:
         {
             return;
         }
-        helper(ans,candidates,target,sum,i+1,x);
+        helper(candidates,target,sum,i+1,x);
         
         x.push_back(candidates[i]);
         
-        helper(ans,candidates,target,sum+candidates[i],i,x);
+        helper(candidates,target,sum+candidates[i],i,x);
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
         vector<int> x;
-        helper(ans,candidates,target,0,0,x);
+        helper(candidates,target,0,0,x);
         return ans;
         
     }
