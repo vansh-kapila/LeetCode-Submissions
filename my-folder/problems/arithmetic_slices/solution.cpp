2047798c0b785076a8e7950b6393f48af6ca92fc
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-03-03 15:04:19 +0000

    Sync LeetCode submission - Arithmetic Slices (cpp)

--- my-folder/problems/arithmetic_slices/solution.cpp
+++ my-folder/problems/arithmetic_slices/solution.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        if(nums.size()<=2)
-        {
-            return 0;
-        }
-        int n = nums.size();
-        int k = 0;
+        
         int ans = 0;
-        for(int i=0;i<n-2;i++)
-        {
-            if(nums[i+2]-nums[i+1]==nums[i+1]-nums[i])
-            {
-                k++;
-                ans+=k;
-            }
-            else
-            {
-                k=0;
-            }
+        for(int i=0;i<nums.size();i++)
+        {   int j = 1;
+            while(i>0 and i<nums.size()-1 and nums[i+1]-nums[i]==nums[i]-nums[i-1])
+            {    
+                i++;
+                ans = ans+(j++);
+            }  
         }
         return ans;
     }
