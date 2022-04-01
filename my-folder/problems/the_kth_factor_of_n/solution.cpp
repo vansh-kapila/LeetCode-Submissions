fabd18c9c9e0dd1fd3f1431250725c853d744427
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-04-01 09:22:07 +0000

    Sync LeetCode submission - The kth Factor of n (cpp)

--- my-folder/problems/the_kth_factor_of_n/solution.cpp
+++ my-folder/problems/the_kth_factor_of_n/solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        
-        for(int i=1;i<=n;i++)
+        set<int> s;
+        for(int i=1;i<=sqrt(n);i++)
         {
             if(n%i==0)
             {
-                k--;
-            }
-            if(k==0)
-            {
-                return i;
+                s.insert(i);
+                s.insert(n/i);
             }
         }
-        
-        return -1;
+        auto it = s.begin(); 
+        if(s.size()<k)
+        {
+            return -1;
+        } 
+        it = next(it,k-1);
+        return *it;
     }
 };
