eba9b4b32f6620e38fe917deb19943184ede42cf
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-04-02 13:19:03 +0000

    Sync LeetCode submission - Valid Palindrome II (cpp)

---
+++ my-folder/problems/valid_palindrome_ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validPalindrome(string &s) {
+          int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) return validPalin(s, left, right - 1) || validPalin(s, left + 1, right);
+            left++;
+            right--;
+        }
+        return true;
+    }
+        bool validPalin(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left++] != s[right--]) return false;
+        }
+        return true;
+    }
+
+};
