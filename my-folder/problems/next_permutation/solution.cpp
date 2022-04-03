db8eeeaf77399a2d72aba531766a5d9e0cf7d9b4
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-04-03 05:33:37 +0000

    Sync LeetCode submission - Next Permutation (cpp)

--- my-folder/problems/next_permutation/solution.cpp
+++ my-folder/problems/next_permutation/solution.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        cout<<next_permutation(nums.begin(),nums.end())<<endl;
+        multiset<int> s;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            s.insert(nums[i]);
+            auto it = s.upper_bound(nums[i]);
+            if(it!=s.end())
+            {   nums[i]=*it;
+                s.erase(it);
+                for(int j=i+1;j<nums.size();j++)
+                {
+                    nums[j] = *s.begin();
+                    s.erase(s.begin());
+                }
+                break;
+            }
+        }
+        if(!s.empty())
+        {
+            sort(nums.begin(),nums.end());
+        }
     }
 };
