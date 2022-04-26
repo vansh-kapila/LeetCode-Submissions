a90d1ed14799ef8166372b8411b06441b7165e97
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-04-26 13:53:50 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (cpp)

--- my-folder/problems/minimum_size_subarray_sum/solution.cpp
+++ my-folder/problems/minimum_size_subarray_sum/solution.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int index = -1;
+    int minSubArrayLen(int target, vector<int>& nums) { 
+        int l = 0;
+        int r = 0;
         int sum = 0;
+        int ans = 1e9+7;
+        int k = target;
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
-            if(sum>=target)
-            {
-                index = i;
-                break;
+            r=i;
+            if(sum>=k)
+            {   
+                while(l<r and sum-nums[l]>=k)
+                {      
+                    sum=sum-nums[l];
+                    l++;
+                }
+                ans = min(ans,r-l+1);
             }
         }
-        if(index==-1)
+        if(ans==1000000007)
         {
             return 0;
         }
-        int j = 0;
-        int ans = nums.size();
-        for(int i=index+1;i<nums.size();i++)
-        {
-            while(sum-nums[j]>=target)
-            {
-                sum=sum-nums[j];
-                j++;
-            }
-            ans = min(i-j,ans);
-            sum = sum+nums[i];
-        }
-        while(sum-nums[j]>=target)
-            {
-                sum=sum-nums[j];
-                j++;
-            }
-        int x = nums.size();
-        ans=min(ans,x-j);
-        return ans;
+        return ans; 
     }
 };
