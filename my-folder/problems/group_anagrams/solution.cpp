6922d691959644b0b4cb0a103cb529e9597ce94c
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-05-28 05:25:48 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- my-folder/problems/group_anagrams/solution.cpp
+++ my-folder/problems/group_anagrams/solution.cpp
@@ -1,31 +1,26 @@
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
-        vector<pair<string,string>> v;
-        for(int i=0;i<strs.size();i++)
+class Solution
+{
+    public:
+        vector<vector < string>> groupAnagrams(vector<string> &strs)
         {
-            string x = strs[i];
-            sort(x.begin(),x.end());
-            v.push_back(make_pair(x,strs[i]));
+            map<string, vector < string>> m;
+            for (auto &it: strs)
+            {
+                string temp = it;
+                sort(temp.begin(), temp.end());
+                m[temp].push_back(it);
+            }
+            vector<vector < string>> ans(m.size());
+            
+            int j = 0;
+            for (auto &it: m)
+            {
+                for (int i = 0; i < it.second.size(); i++)
+                {
+                    ans[j].push_back(it.second[i]);
+                }
+                j++;
+            }
+            return ans;
         }
-        
-        sort(v.begin(),v.end());
-        
-        vector<vector<string>> ans;
-        vector<string> temp;
-        int i = 0;
-        while(i<strs.size())
-        {   
-            temp.clear();
-            string x = v[i].first;
-            while(i<strs.size() and v[i].first==x)
-            {         
-                temp.push_back(v[i].second);
-                i++;
-            } 
-            ans.push_back(temp);
-        }
-        return ans;
-    }
 };
