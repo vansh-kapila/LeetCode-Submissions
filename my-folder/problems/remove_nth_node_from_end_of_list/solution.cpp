f26741300be4c0c4891b0e37c36bdc1fa4234af6
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-06-02 13:42:07 +0000

    Sync LeetCode submission - Remove Nth Node From End of List (cpp)

--- my-folder/problems/remove_nth_node_from_end_of_list/solution.cpp
+++ my-folder/problems/remove_nth_node_from_end_of_list/solution.cpp
@@ -1,41 +1,37 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* ptr = head;
-        if(head==NULL or head->next==NULL)
-        {
-            return NULL;
+class Solution
+{
+    public:
+        ListNode* removeNthFromEnd(ListNode *head, int n)
+        { 
+            ListNode *slow = head;
+            ListNode *fast = head;
+
+            while (fast != nullptr)
+            {
+                if (n >= 0)
+                {
+                    n--;
+                    fast = fast->next;
+                    continue;
+                }
+                fast = fast->next;
+                slow = slow->next;
+            }
+            if(n>=0)
+            {   
+                return head->next;
+            }
+            slow->next = slow->next->next;
+            return head;
         }
-        int count = 0;
-        while(ptr!=NULL)
-        {
-            ptr=ptr->next;
-            count++;
-        }
-        if(count==n)
-        {
-            return head->next;
-        }
-        count = count-n;
-        ListNode* x = head; 
-        count--;
-        while(count--)
-        {
-            x = x->next;
-        }
-        
-        x->next = x->next->next;
-        
-        return head;
-    }
 };
