aa72cd78b47e176ae4f67977d6354d3aeec7ee2a
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-06-02 07:49:59 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- my-folder/problems/merge_two_sorted_lists/solution.cpp
+++ my-folder/problems/merge_two_sorted_lists/solution.cpp
@@ -1,56 +1,47 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* anss;
-        anss=new ListNode(0);
-        ListNode* ans = anss;
-
-        while(l1!=NULL and l2!=NULL)
+class Solution
+{
+    public:
+        ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
         {
-            if(l1->val<=l2->val)
+            ListNode head(INT_MIN);
+            ListNode* newList = &head;
+            while (list1 != nullptr and list2 != nullptr)
             {
-                ans->next = l1;
-                l1=l1->next;
-                ans = ans->next;
+                if (list1->val < list2->val)
+                {
+                    newList->next = list1;
+                    list1 = list1->next;
+                    newList = newList->next;
+                }
+                else
+                {
+                    newList->next = list2;
+                    list2 = list2->next;
+                    newList = newList->next;
+                }
             }
-            else
+            while (list1 != nullptr)
             {
-                ans->next=l2;
-                l2=l2->next;
-                ans = ans->next;
+                newList->next = list1;
+                list1 = list1->next;
+                newList = newList->next;
             }
+            while (list2 != nullptr)
+            {
+                newList->next = list2;
+                list2 = list2->next;
+                newList = newList->next;
+            }
+            return head.next;
         }
-        
-        if(l1!=NULL)
-        {
-            while(l1!=NULL)
-            {
-                ans->next = l1;
-                l1 = l1->next;
-                ans=ans->next;
-            }
-        }
-        
-         else if(l2!=NULL)
-        {
-            while(l2!=NULL)
-            {
-                ans->next = l2;
-                l2 = l2->next;
-                ans=ans->next;
-            }
-        }
-        
-        return anss->next;
-    }
 };
