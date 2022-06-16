75c6921f4899d722498bc8d4275f99109a258ab9
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-06-16 05:26:38 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (cpp)

--- my-folder/problems/letter_combinations_of_a_phone_number/solution.cpp
+++ my-folder/problems/letter_combinations_of_a_phone_number/solution.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    vector<string> v = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    vector<string> store = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> ans;
     void solve(string &digits,string &str,int i)
     {
-        if(i==str.length())
-        {   
+        if(i==digits.length())
+        {
             ans.push_back(str);
             return;
         }
-        char c = digits[i];
-        for(int j=0;j<v[c-'0'].size();j++)
-        {   
-            str[i]=v[c-'0'][j];
-            solve(digits,str,i+1);
+        for(auto j:store[digits[i]-'0'])
+        {
+            str.push_back(j);
+            solve(digits,str,i+1); 
+            str.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        if(digits.empty())
+         if(digits.empty())
         {
             return {};
         }
-        string str = digits;
+        string str;
         solve(digits,str,0);
         return ans;
     }
