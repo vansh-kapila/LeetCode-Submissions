335943cb8d1a7f1dd7773614eb4bf3e29f54842f
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-01 08:30:26 +0000

    Sync LeetCode submission - Minimum Difference Between Largest and Smallest Value in Three Moves (cpp)

---
+++ my-folder/problems/minimum_difference_between_largest_and_smallest_value_in_three_moves/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        if(nums.size()<=3)
+        {
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+        int n = nums.size()-1;
+        return min({nums[n-3]-nums[0],nums[n-2]-nums[1],nums[n-1]-nums[2],nums[n]-nums[3]});
+    }
+};
