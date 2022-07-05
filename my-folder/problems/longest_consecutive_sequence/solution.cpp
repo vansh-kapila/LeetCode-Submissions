8193a29df336620851bd69d516f1547a5c3844ad
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-05 06:43:38 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- my-folder/problems/longest_consecutive_sequence/solution.cpp
+++ my-folder/problems/longest_consecutive_sequence/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> hashset(nums.begin(),nums.end());
+        unordered_set<int> st(nums.begin(),nums.end());
+        int count = 0;
         int ans = 0;
-        for(auto i:hashset)
+        for(int i=0;i<nums.size();i++)
         {
+            int j = nums[i];
             int count = 1;
-            int j = i;
-            while(hashset.find(j+1)!=hashset.end())
-            {   
-                hashset.erase(hashset.find(++j)); 
-                count++;
+            while(st.find(j+1)!=st.end())
+            {
+                count++; 
+                st.erase(st.find(j+1));
+                j++;
             }
-            j=i;
-            while(hashset.find(j-1)!=hashset.end())
-            {   
-                hashset.erase(hashset.find(--j)); 
-                count++;
+            int y = nums[i];
+            while(st.find(y-1)!=st.end())
+            {
+                count++; 
+                st.erase(st.find(y-1));
+                y--;
             }
             ans = max(ans,count);
         }
