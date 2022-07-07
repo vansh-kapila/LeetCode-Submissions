dbfd20243b03ab823f597e67bd6b49af6d7d8770
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-07 03:32:23 +0000

    Sync LeetCode submission - Spiral Matrix (cpp)

---
+++ my-folder/problems/spiral_matrix/solution.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+    public:
+        vector<int> spiralOrder(vector<vector < int>> &matrix)
+        {
+            int top = 0;
+            int right = matrix[0].size() - 1;
+            int bottom = matrix.size() - 1;
+            int left = 0;
+
+            vector<int> v;
+            while (top <= bottom and left <= right)
+            {
+                for (int i = left; i <= right; i++)
+                {
+                    v.push_back(matrix[top][i]);
+                }
+                top++;
+                if (right >= left)
+                {
+                    for (int i = top; i <= bottom; i++)
+                    {
+                        v.push_back(matrix[i][right]);
+                    }
+                    right--;
+                }
+                if (top <= bottom)
+                {
+                    for (int i = right; i >= left; i--)
+                    {
+                        v.push_back(matrix[bottom][i]);
+                    }
+                    bottom--;
+                }
+                if (right >= left)
+                {
+                    for (int i = bottom; i>=top ;i--)
+                    {
+                        v.push_back(matrix[i][left]);
+                    }
+                    left++;
+                }
+            }
+            return v;
+        }
+};
