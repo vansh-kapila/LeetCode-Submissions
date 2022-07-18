cb1da8cc0d8871897bbd39b63aaf2e4ebe5ed253
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-18 20:09:02 +0000

    Sync LeetCode submission - Implement Queue using Stacks (cpp)

--- my-folder/problems/implement_queue_using_stacks/solution.cpp
+++ my-folder/problems/implement_queue_using_stacks/solution.cpp
@@ -1,28 +1,25 @@
-class MyQueue
-{
-    public:
-        stack<int> s1;
+class MyQueue {
+public:
+    stack<int> s1;
     stack<int> s2;
-    int front = 0;
-    MyQueue() {}
-
-    void push(int x)
-    {
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
         s1.push(x);
     }
-
-    int pop()
-    {
+    
+    int pop() {
         int x = peek();
         s2.pop();
         return x;
     }
-
-    int peek()
-    {
-        if (s2.empty())
+    
+    int peek() {
+        if(s2.empty())
         {
-            while (!s1.empty())
+            while(!s1.empty())
             {
                 s2.push(s1.top());
                 s1.pop();
@@ -30,18 +27,17 @@ class MyQueue
         }
         return s2.top();
     }
-
-    bool empty()
-    {
-        return (s1.empty()) && (s2.empty());
+    
+    bool empty() {
+        return s1.empty()&s2.empty();
     }
 };
 
 /**
- *Your MyQueue object will be instantiated and called as such:
- *MyQueue* obj = new MyQueue();
- *obj->push(x);
- *int param_2 = obj->pop();
- *int param_3 = obj->peek();
- *bool param_4 = obj->empty();
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
  */
