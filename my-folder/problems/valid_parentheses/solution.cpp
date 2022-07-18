fa5fd526791d4b727be4676885f64bb2aaad24fc
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-18 19:23:09 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- my-folder/problems/valid_parentheses/solution.cpp
+++ my-folder/problems/valid_parentheses/solution.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> ss;
-        int n = s.length();
-        bool ans = true;
-        for(int i=0;i<n;i++)
+        stack<char> st;
+        for(auto i:s)
         {
-            if(s[i]=='(' or s[i]=='{' or s[i]=='[')
+            if(i=='(' or i=='{' or i=='[')
             {
-                ss.push(s[i]);
+                st.push(i);
+            }
+            else if(!st.empty() and i==')' and st.top()!='(')
+            {
+                return 0;
+            }
+            else if(!st.empty() and i==')' and st.top()=='(')
+            {
+                st.pop();
+            }
+            else if(!st.empty() and i=='}' and st.top()!='{')
+            {
+                return 0;
+            }
+            else if(!st.empty() and i=='}' and st.top()=='{')
+            {
+                st.pop();
+            }
+            else if(!st.empty() and i==']' and st.top()!='[')
+            {
+                return 0;
+            }
+            else if(!st.empty() and i==']' and st.top()=='[')
+            {
+                st.pop();
             }
             else
-            {  if(ss.empty()==true)
-               {
-                ans = false;
-                break;
-               }
-                else if(s[i]==')' and ss.top()=='(')
-                {
-                    ss.pop();
-                }
-                else if(s[i]==']' and ss.top()=='[')
-                {
-                    ss.pop();
-                }
-                else if(s[i]=='}' and ss.top()=='{')
-                {
-                    ss.pop();
-                }
-                else
-                {
-                    ans = false;
-                    break;
-                }
-                                
+            {
+                st.push(i);
             }
         }
-        
-        if(ans==false or ss.empty()==false)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+        return st.empty();
     }
 };
