301be5c35e2b5f31cf52f8cc3ee66ff13a34a62c
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-18 20:18:12 +0000

    Sync LeetCode submission - Middle of the Linked List (cpp)

--- my-folder/problems/middle_of_the_linked_list/solution.cpp
+++ my-folder/problems/middle_of_the_linked_list/solution.cpp
@@ -10,14 +10,13 @@
  */
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode* fast = head;
+    ListNode* middleNode(ListNode* head) { 
         ListNode* slow = head;
-        
+        ListNode* fast = head;
         while(fast!=nullptr and fast->next!=nullptr)
         {
-            fast = fast->next->next;
-            slow = slow->next;
+            fast=fast->next->next;
+            slow=slow->next;
         }
         return slow;
     }
