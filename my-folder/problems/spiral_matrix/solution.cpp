4621f0629352c11ccef2df1b970c91722db7e255
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-19 03:43:49 +0000

    Sync LeetCode submission - Spiral Matrix (cpp)

--- my-folder/problems/spiral_matrix/solution.cpp
+++ my-folder/problems/spiral_matrix/solution.cpp
@@ -1,46 +1,35 @@
-class Solution
-{
-    public:
-        vector<int> spiralOrder(vector<vector < int>> &matrix)
-        {
-            int top = 0;
-            int right = matrix[0].size() - 1;
-            int bottom = matrix.size() - 1;
-            int left = 0;
-
-            vector<int> v;
-            while (top <= bottom and left <= right)
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> v;
+        int top = 0,bottom = matrix.size()-1,left = 0,right = matrix[0].size()-1;
+        while(top<=bottom and left<=right)
+        { 
+            for(int i=left;i<=right;i++)
             {
-                for (int i = left; i <= right; i++)
-                {
-                    v.push_back(matrix[top][i]);
-                }
-                top++;
-                if (right >= left)
-                {
-                    for (int i = top; i <= bottom; i++)
-                    {
-                        v.push_back(matrix[i][right]);
-                    }
-                    right--;
-                }
-                if (top <= bottom)
-                {
-                    for (int i = right; i >= left; i--)
-                    {
-                        v.push_back(matrix[bottom][i]);
-                    }
-                    bottom--;
-                }
-                if (right >= left)
-                {
-                    for (int i = bottom; i>=top ;i--)
-                    {
-                        v.push_back(matrix[i][left]);
-                    }
-                    left++;
-                }
+                v.push_back(matrix[top][i]);
             }
-            return v;
+            top++; 
+            for(int i=top;i<=bottom;i++)
+            {
+                v.push_back(matrix[i][right]);
+            }
+            if(top<=bottom){
+            right--;
+            for(int i=right;i>=left;i--)
+            {
+                v.push_back(matrix[bottom][i]);
+            }
+            }
+            if(left<=right){
+            bottom--;
+            for(int i=bottom;i>=top;i--)
+            {
+                v.push_back(matrix[i][left]);
+            }
+                left++;
+            }
         }
+        return v;
+    }
 };
