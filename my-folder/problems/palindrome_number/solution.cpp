41396fcbd83f8d521ae92d5e89f5db77b86483e5
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-19 02:50:44 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

--- my-folder/problems/palindrome_number/solution.cpp
+++ my-folder/problems/palindrome_number/solution.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long a=0;
-        long long temp = x;
-        while(x!=0)
-        {
-            a=a*10+x%10;
-            x=x/10;
-        }
-        
-        if(a==temp and temp>=0)
-            return true;
-        else
-            return false;
+        string s = to_string(x);
+        string rev = s;
+        reverse(rev.begin(),rev.end());
+        return s==rev;
     }
 };
