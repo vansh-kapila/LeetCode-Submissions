6f5787c310393f401003dd91dcec7e13d92aab5a
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-19 02:39:37 +0000

    Sync LeetCode submission - Product of Array Except Self (cpp)

--- my-folder/problems/product_of_array_except_self/solution.cpp
+++ my-folder/problems/product_of_array_except_self/solution.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
-        int arr[nums.size()];
-        int brr[nums.size()];
-        int x = 1;
         int n = nums.size();
-        for(int i=0;i<n;i++)
+        vector<int> suff(n+1,1);
+        vector<int> pre(n+1,1);
+        for(int i=0;i<nums.size();i++)
         {
-            x = x*nums[i];
-            arr[i]=x;
+            pre[i+1]=pre[i]*nums[i];
         }
-        
-        x = 1;
-        for(int i=n-1;i>=0;i--)
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            x = x*nums[i];
-            brr[i]=x;
+            suff[i]=suff[i+1]*nums[i];
         }
-        nums[0]=brr[1];
-        nums[nums.size()-1] = arr[nums.size()-2];
-        for(int i=1;i<n-1;i++)
+        vector<int> ans(nums.size());
+        for(int i=0;i<nums.size();i++)
         {
-            nums[i]=arr[i-1]*brr[i+1];
+            ans[i]=pre[i]*suff[i+1];
         }
-        return nums;
-        
+        return ans;
     }
 };
