0a94d26b5f83fd9225629387d466434ddf3d014a
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-19 03:05:55 +0000

    Sync LeetCode submission - Number of Islands (cpp)

--- my-folder/problems/number_of_islands/solution.cpp
+++ my-folder/problems/number_of_islands/solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        vector<vector<bool>> vis(grid.size(),vector<bool>(grid[0].size(),0));
         int count = 0;
         for(int i=0;i<grid.size();i++)
         {
             for(int j=0;j<grid[i].size();j++)
             {
-                if(grid[i][j]=='1')
+                if(grid[i][j]=='1' and !vis[i][j])
                 {
                     queue<pair<int,int>> q;
                     q.push({i,j});
@@ -14,25 +15,25 @@ public:
                     {
                         auto it = q.front();
                         q.pop();
-                        if(it.first+1<grid.size() and grid[it.first+1][it.second]=='1')
+                        if(it.first>=1 and !vis[it.first-1][it.second] and grid[it.first-1][it.second]=='1')
                         {
-                            q.push({it.first+1,it.second});
-                            grid[it.first+1][it.second]='2';
-                        }
-                        if(it.first-1>=0 and grid[it.first-1][it.second]=='1')
-                        {
+                            vis[it.first-1][it.second]=1;
                             q.push({it.first-1,it.second});
-                            grid[it.first-1][it.second]='2';
                         }
-                        if(it.second+1<grid[0].size() and grid[it.first][it.second+1]=='1')
+                        if(it.second>=1 and !vis[it.first][it.second-1] and grid[it.first][it.second-1]=='1')
                         {
-                            q.push({it.first,it.second+1});
-                            grid[it.first][it.second+1]='2';
-                        }
-                        if(it.second-1>=0 and grid[it.first][it.second-1]=='1')
-                        {
+                            vis[it.first][it.second-1]=1;
                             q.push({it.first,it.second-1});
-                            grid[it.first][it.second-1]='2';
+                        }
+                        if(it.first+1<grid.size() and !vis[it.first+1][it.second] and grid[it.first+1][it.second]=='1')
+                        {
+                            vis[it.first+1][it.second]=1;
+                            q.push({it.first+1,it.second});
+                        }
+                        if(it.second+1<grid[0].size() and !vis[it.first][it.second+1] and grid[it.first][it.second+1]=='1')
+                        {
+                            vis[it.first][it.second+1]=1;
+                            q.push({it.first,it.second+1});
                         }
                     }
                     count++;
