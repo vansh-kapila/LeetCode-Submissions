1a70b8e0b314fb8139cb243fa3b846c2b4e56a85
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-20 04:35:09 +0000

    Sync LeetCode submission - Clone Graph (cpp)

--- my-folder/problems/clone_graph/solution.cpp
+++ my-folder/problems/clone_graph/solution.cpp
@@ -1,26 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
 class Solution {
 public:
-    Node* cloneGraph(Node* node) {
-        if (!node) {
-            return NULL;
+    map<Node*,Node*> mp;
+    Node* dfs(Node* root)
+    {
+        if(root==nullptr)
+        {
+            return nullptr;
         }
-        Node* copy = new Node(node -> val, {});
-        copies[node] = copy;
-        queue<Node*> todo;
-        todo.push(node);
-        while (!todo.empty()) {
-            Node* cur = todo.front();
-            todo.pop();
-            for (Node* neighbor : cur -> neighbors) {
-                if (copies.find(neighbor) == copies.end()) {
-                    copies[neighbor] = new Node(neighbor -> val, {});
-                    todo.push(neighbor);
-                }
-                copies[cur] -> neighbors.push_back(copies[neighbor]);
+        
+        if(mp.find(root)==mp.end())
+        {
+            mp[root] = new Node(root->val,{});
+            for(auto i:root->neighbors)
+            {
+                mp[root]->neighbors.push_back(dfs(i));
             }
         }
-        return copy;
+        return mp[root];
     }
-private:
-    unordered_map<Node*, Node*> copies;
+    Node* cloneGraph(Node* node) {
+        return dfs(node); 
+    }
 };
