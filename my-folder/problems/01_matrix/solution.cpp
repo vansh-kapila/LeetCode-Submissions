239167358ba98be79637e56ef49786976dee8a5a
Vansh Kapila <72248500+vansh-kapila@users.noreply.github.com>
2022-07-22 18:57:17 +0000

    Sync LeetCode submission - 01 Matrix (cpp)

--- my-folder/problems/01_matrix/solution.cpp
+++ my-folder/problems/01_matrix/solution.cpp
@@ -1,58 +1,107 @@
-class Solution
-{
-    public:
-
-        vector<vector < int>> updateMatrix(vector<vector < int>> &mat)
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<vector<int>> dp(mat.size(),vector<int>(mat[0].size(),1e9));
+        queue<pair<int,int>> q;
+        for(int i=0;i<mat.size();i++)
         {
-            vector<vector < int>> dp(mat.size(), vector<int> (mat[0].size(), 1e9+7));
-
-            for (int i = 0; i < mat.size(); i++)
+            for(int j=0;j<mat[0].size();j++)
             {
-                for (int j = 0; j < mat[0].size(); j++)
+                if(mat[i][j]==0)
                 {
-                    if (mat[i][j] == 0)
-                    {
-                        dp[i][j] = 0;
-                    }
-                    else
-                    {
-                        int leftmin=1e9, downmin=1e9;
-                        if (j >= 1)
-                        {
-                            leftmin = dp[i][j - 1] + 1;
-                        }
-                        if (i >= 1)
-                        {
-                            downmin = dp[i - 1][j] + 1;
-                        }
-                        dp[i][j] = min({ dp[i][j],leftmin,downmin});
-                    }
+                    q.push({i,j});
+                    dp[i][j]=0;
                 }
             }
-            
-            for (int i = mat.size()-1; i>=0; i--)
-            {
-                for (int j = mat[0].size()-1; j >=0; j--)
-                {
-                    if (mat[i][j] == 0)
-                    {
-                        dp[i][j] = 0;
-                    }
-                    else
-                    {
-                        int rightmin=1e9, upmin=1e9;
-                        if (j<mat[0].size()-1)
-                        {
-                            rightmin = dp[i][j + 1] + 1;
-                        }
-                        if (i<mat.size()-1)
-                        {
-                            upmin = dp[i + 1][j] + 1;
-                        }
-                        dp[i][j] = min({ dp[i][j],upmin,rightmin});
-                    }
-                }
-            }
-            return dp;
         }
+        int ans = 0;
+        while(!q.empty())
+        {
+            ans++;
+            int n = q.size();
+            while(n--)
+            {
+                auto it = q.front();
+                q.pop();
+                if(it.first-1>=0 and mat[it.first-1][it.second]==1)
+                {
+                    mat[it.first-1][it.second]=2;
+                    q.push({it.first-1,it.second});
+                    dp[it.first-1][it.second]=min(dp[it.first-1][it.second],ans);
+                }
+                if(it.second-1>=0 and mat[it.first][it.second-1]==1)
+                {
+                    mat[it.first][it.second-1]=2;
+                    q.push({it.first,it.second-1});
+                    dp[it.first][it.second-1]=min(dp[it.first][it.second-1],ans);
+                }
+                 if(it.first+1<mat.size() and mat[it.first+1][it.second]==1)
+                {
+                    mat[it.first+1][it.second]=2;
+                    q.push({it.first+1,it.second});
+                    dp[it.first+1][it.second]=min(dp[it.first+1][it.second],ans);
+                }
+                if(it.second+1<mat[0].size() and mat[it.first][it.second+1]==1)
+                {
+                    mat[it.first][it.second+1]=2;
+                    q.push({it.first,it.second+1});
+                    dp[it.first][it.second+1]=min(dp[it.first][it.second+1],ans);
+                }
+            }
+        }
+        
+//         for(int i=0;i<mat.size();i++)
+//         {
+//             for(int j=0;j<mat[0].size();j++)
+//             {
+//                 if(mat[i][j]==0)
+//                 {
+//                     q.push({i,j});
+//                 }
+//                 if(mat[i][j]==2)
+//                 {
+//                     mat[i][j]=1;
+//                 }
+//             }
+//         }
+        
+//         ans = 0;
+//         while(!q.empty())
+//         {
+//             ans++;
+//             int n = q.size();
+//             while(n--)
+//             {
+//                 auto it = q.front();
+//                 q.pop();
+//                 if(it.first+1<mat.size() and mat[it.first+1][it.second]==1)
+//                 {
+//                     mat[it.first+1][it.second]=2;
+//                     q.push({it.first+1,it.second});
+//                     if(dp[it.first+1][it.second]!=0)
+//                     {
+//                         dp[it.first+1][it.second]=min(dp[it.first+1][it.second],ans);
+//                     }
+//                     else
+//                     {
+//                         dp[it.first+1][it.second] = ans;
+//                     }
+//                 }
+//                 if(it.second+1<mat[0].size() and mat[it.first][it.second+1]==1)
+//                 {
+//                     mat[it.first][it.second+1]=2;
+//                     q.push({it.first,it.second+1}); 
+//                     if(dp[it.first][it.second+1]!=0)
+//                     {
+//                         dp[it.first][it.second+1]=min(dp[it.first][it.second+1],ans);
+//                     }
+//                     else
+//                     {
+//                         dp[it.first][it.second+1] = ans;
+//                     }
+//                 }
+//             }
+//         }
+        return dp;
+    }
 };
